522a6af0ce563fecdede7a289b65b2a78c2d205d
thierry abbey <habbey480@gmail.com>
2024-11-28 10:45:46 +0100

    Déclarer les variables, tableaux et définir les structures  énumérations et union

--- Transgerbeur/transgerbeurcpp.cpp
+++ Transgerbeur/transgerbeurcpp.cpp
@@ -1,6 +1,27 @@
 #include <iostream>
-
 using namespace std;
-int maqin() {
-	cout << "Transgerbeur" << endl;
+enum EEtat {
+	vide, palette, rouleau
+};
+struct SPalette
+{
+	int poids;
+	char ref[21];
+};
+struct SRouleau {
+	float longeur;
+	int numero;
+};
+union Ucontenu {
+	SPalette palette;
+	SRouleau rouleau;
+};
+struct Scasier {
+	int px;
+	int py;
+	SPalette palette;
+	SRouleau rouleau;
+};
+int main() {
+	cout << "Le peograme Transgerbeur" << endl;
 }
